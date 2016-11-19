3ff2cfe5d3ec24ff27a3a4215d407082e994c9e9
Courtney Luk <courtneyluk@Courtney-Luks-MacBook-Pro-2.local>
2016-11-19 14:49:13 -0500

    opens game window now

--- pygame_final.py
+++ pygame_final.py
@@ -4,3 +4,13 @@ import pygame
 pygame.init();
 
 gameDisplay = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Those Darn Blocks")
+
+pygame.display.update()		#only updates portion specified
+
+
+gameExit = False
+while not gameExit:
+	for event in pygame.event.get():
+		print(event)
