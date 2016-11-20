ff5eb256142a38f729f7429071ddc43821857abb
Courtney Luk <courtneyluk@0587347321.wireless.umich.net>
2016-11-20 15:31:39 -0500

    colors defined

--- pygame_final.py
+++ pygame_final.py
@@ -3,6 +3,15 @@
 import pygame
 pygame.init();
 
+
+#create colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+
 gameDisplay = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("Those Darn Blocks")
@@ -14,3 +23,6 @@ gameExit = False
 while not gameExit:
 	for event in pygame.event.get():
 		print(event)
+
+def dra
+
